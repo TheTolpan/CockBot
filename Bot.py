21d9e33f89dc8d920c5faf286550870a35b9553b
TheTolpan <52505959+TheTolpan@users.noreply.github.com>
2020-04-06 15:08:12 +0200

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,23 @@
+import discord
+import random
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print('Cock is ready.')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'{round(client.latency * 1000)}ms')
+
+@client.command()
+async def huso(ctx):
+    responses = ['Bist du behindert?',
+                 'Amina Sikerim!',
+                 'Selbst Lars würde dich beleidigen.',
+                 'Selbst Dobos Mutter würde dich von der Bettkante schupsen.']
+    await ctx.send(f'{random.choice(responses)}')
+
+client.run('Njk2NjgzMjQ0NDUxNjU5Nzg2.XosYkw.8SggqPoQ7O23gLnmdt2I5E0MmNE')
